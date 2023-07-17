7fe21212319986429afa7868af6e71e1f16b1ebe
nisharamananathan <78706007+nisharamananathan@users.noreply.github.com>
2023-07-16 19:37:31 -0500

    more edits

--- picture 1.js
+++ picture 1.js
@@ -1,44 +1,44 @@
-// clean and create data
-var response = await fetch("Titanic-Dataset.csv");
-var data = await response.text();
-data = d3.csvParse(data);
+// chart #1: pie chart
 
-data.forEach(function(d) {
-    d.Survived = +d.Survived 
-});
+async function fetchData() {
+    var response = await fetch("Titanic-Dataset.csv");
+    var data = await response.text();
+    data = d3.csvParse(data);
 
-// variables and initiate pie chart
-var survived = d3.sum(data, function(d) {return d.Survived; });
-var not_survived = data.length - survived;
+    data.forEach(function(d) {
+        d.Survived = +d.Survived;
+    });
 
-var color = ['green', 'red'];
+    var color = ['green', 'red'];
 
-var pieData = [
-    {label: "Survived", count: survived},
-    {label: "Not Survived", count: not_survived }
-];
+    var survived = d3.sum(data, function(d) { return d.Survived; });
+    var not_survived = data.length - survived;
 
-// pie chart 
-var width = 400;
-var height = 400;
-var radius = 200;
+    var pieData = [
+        { label: "Survived", count: survived },
+        { label: "Not Survived", count: not_survived }
+    ];
 
-var pie = d3.pie().value(function(d) {return d.count; });
-var arc = d3.arc()
-    .innerRadius(0)
-    .outerRadius(radius);
+    var width = 400;
+    var height = 400;
+    var radius = 200;
 
-var g = d3.select("#chart")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", "translate(200,200)");
+    var pie = d3.pie().value(function(d) { return d.count; });
+    var arc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius);
 
-g.selectAll("arc")
-    .data(pie(data))
-    .enter()
-    .append("path")
-    .attr("d", "arc")
-    .attr("d", arc)
-    .attr("fill", function(d,i) {return color[i]; })
+    var svg = d3.select("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    var g = svg.append("g").attr("transform", "translate(200,200)")
+
+    g.selectAll("arc")
+        .data(pie(pieData))
+        .enter()
+        .append("path")
+        .attr("d", arc)
+        .attr("fill", function(d, i) { return color[i]; });
+}
+fetchData();
